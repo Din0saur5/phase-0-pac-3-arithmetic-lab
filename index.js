367d75ec7f47864bca7a556c2b6ef6edf05b4681
Dan Hackworth <danielhackworth515@gmail.com>
2023-09-11 17:43:30 -0400

    add last two functions, complete lab

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 function add(a,b) {
-    return a+b
+    return a+b;
 }
 
 function subtract(a,b){
-    return a-b
+    return a-b;
 }
 
 function multiply(a,b){
-    return a*b
+    return a*b;
 }
 
 function divide(a,b){
-    return a/b
+    return a/b;
 
 }
 
 function increment(n){
-    n += 1
-    return n
+    n += 1;
+    return n;
 }
 
 function decrement(n){
-    n -= 1
+    n -= 1;
+    return n;
+}
+
+function makeInt(string){
+    let n = parseInt(string, 10);
     return n
+}
+
+function preserveDecimal(string){
+   let n = parseFloat(string);
+    return n;
+
 }
